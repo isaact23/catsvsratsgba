3dbccecc4a81c601371142a846d2fa541bb9d6a1
Isaac Thompson <shufflesninja@gmail.com>
2022-11-02 19:49:07 -0400

    Move projectile properties to projectile_manager

--- include/cats/projectile_manager.h
+++ include/cats/projectile_manager.h
@@ -9,6 +9,20 @@
 
 #define PROJECTILE_LIMIT 10
 
+#define HIT_RADIUS 6
+
+#define PROJECTILE_PAW_TILE 136
+#define PROJECTILE_ARROW_TILE 140
+#define PROJECTILE_MAGIC_TILE 170
+
+#define PROJECTILE_PAW_SPEED 1
+#define PROJECTILE_ARROW_SPEED 2
+#define PROJECTILE_MAGIC_SPEED 2
+
+#define PROJECTILE_PAW_DAMAGE 1
+#define PROJECTILE_ARROW_DAMAGE 2
+#define PROJECTILE_MAGIC_DAMAGE 3
+
 // Initialize projectile manager
 void projectile_manager_init();
 
