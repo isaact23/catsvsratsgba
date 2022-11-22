89634d4f8bdf45f8e5fe446beee9a7b53111428f
Isaac Thompson <shufflesninja@gmail.com>
2022-11-21 19:59:09 -0500

    Use CHAR_BASE_BLOCK and SCREEN_BASE_BLOCK from devkitpro

--- include/screen/tilemap.h
+++ include/screen/tilemap.h
@@ -6,13 +6,9 @@
 
 #include "data/data_tilemap.h"
 #include "data/image_tiles.h"
+
 #include "gba_types.h"
-
-// Return pointer to one of 4 character blocks
-vu16* char_block(u16 block);
-
-// Return pointer to one of 32 screen blocks
-vu16* screen_block(u16 block);
+#include "gba_video.h"
 
 // Set up tilemap
 void tilemap_init();
--- source/screen/tilemap.c
+++ source/screen/tilemap.c
@@ -2,40 +2,27 @@
 
 #include "screen/tilemap.h"
 
-vu16* bg_palette = (vu16*) 0x5000000;
-vu16* vram       = (vu16*) 0x6000000;
-
-// Return pointer to one of 4 character blocks
-vu16* char_block(u16 block) {
-    return (vu16*) (0x6000000 + (block * 0x800));
-}
-
-// Return pointer to one of 32 screen blocks
-vu16* screen_block(u16 block) {
-    return (vu16*) (0x6000000 + (block * 0x800));
-}
-
 // Set up tilemap
 void tilemap_init() {
     
     // Load palette into palette memory
-    for (int i = 0; i < IMAGE_TILES_PALETTE_SIZE; i++) {
-        bg_palette[i] = IMAGE_TILES_PALETTE[i];
+    for (int i = 0; i < IMAGE_TILES_PALETTE_SIZE; i++) { // 132 bytes fits in palette (1 kilobyte)
+        BG_PALETTE[i] = IMAGE_TILES_PALETTE[i];
     }
 
     // Load image into char block 0, two bytes at a time
-    vu16* dest = char_block(0);
+    vu16* dest = CHAR_BASE_BLOCK(0);
     u16* image = (u16*) IMAGE_TILES_DATA;
     if (IMAGE_TILES_WIDTH * IMAGE_TILES_HEIGHT > 250) {
         exit(1);
     }
-    for (int i = 0; i < IMAGE_TILES_WIDTH * IMAGE_TILES_HEIGHT * 32; i++) {
+    for (int i = 0; i < IMAGE_TILES_WIDTH * IMAGE_TILES_HEIGHT * 32; i++) { // 5760 bytes fits in 1 char block (16 kilobytes)
         dest[i] = image[i];
     }
 
     // Load tile data into screen block 16
-    dest = screen_block(16);
-    for (int i = 0; i < TILEMAP_WIDTH * TILEMAP_HEIGHT; i++) {
+    dest = SCREEN_BASE_BLOCK(16);
+    for (int i = 0; i < TILEMAP_WIDTH * TILEMAP_HEIGHT; i++) { // 640 bytes fits in 1 screen block (2 kilobytes)
         dest[i] = TILEMAP[i];
     }
 }
