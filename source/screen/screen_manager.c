64a2a2ae4aaab8087ae95ec3ab7451041e2fb980
Isaac Thompson <shufflesninja@gmail.com>
2022-11-21 20:00:41 -0500

    Use devkitpro registers

--- source/screen/screen_manager.c
+++ source/screen/screen_manager.c
@@ -1,20 +1,16 @@
 #include "screen/screen_manager.h"
 
-vu16* dispcnt = (vu16*) 0x4000000;
-vu16* bg0cnt  = (vu16*) 0x4000008;
-
-
 // Initialize the screen.
 void screen_manager_init(struct sprite* (*sprite_manager_new_sprite)()) {
     // Set up display control
-    *dispcnt =
+    REG_DISPCNT =
         (0 << 0)  | // Display mode 0
         (0 << 6)  | // 1 for 1D sprite mode, 0 for 2D sprite mode
         (1 << 8)  | // Enable background 0
         (1 << 12);  // Screen display OBJ
 
     // Set up background 0
-    *bg0cnt = (1 << 7)  | // 0 is 16 colors, 1 is 256
+    REG_BG0CNT = (1 << 7)  | // 0 is 16 colors, 1 is 256
               (16 << 8) | // Tile data stored in screen block 16
               (1 << 13) | // Enable wrapping
               (0 << 14);  // Background size, 0 is 256x256 (32x32 tiles)
@@ -38,5 +34,5 @@ void screen_manager_win_gui() {
 
 // Display loss GUI.
 void screen_manager_lose_gui() {
-    
+
 }
